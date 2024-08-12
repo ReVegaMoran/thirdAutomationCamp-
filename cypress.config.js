5b72311f54e49db1c280fb556973dd700330b5cd
Rene Vega <re.vegamoran@gmail.com>
2024-08-12 06:33:35 -0400

    configuration to run automation tests in iPhone XR viewport.

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,14 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
-  e2e: {
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
-    },
-  },
-});
+    chromeWebSecurity: false,
+    isMobile: true,
+    viewportWidth: 414,
+    viewportHeight: 896,
+    e2e: {
+        baseUrl: "https://www.casaideas.cl/",
+        setupNodeEvents(on, config) {
+            // implement node event listeners here
+        }
+    }
+})
